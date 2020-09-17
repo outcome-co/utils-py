a4d3a35514692654c915270d0ca1fefb6065bb63
Douglas Willcocks <douglas@d-tw.org>
2020-09-17 10:46:53 +0200

    feat!: singleton feature registry (#39)
    * feat!: improve feature set
    
    * feat!: improve feature set
    
    * ci: add dependabot prefix
    
    * ci: add cache

--- test/test_feature_set.py
+++ test/test_feature_set.py
@@ -1,9 +1,11 @@
 import os
+import warnings
 from pathlib import Path
 from unittest.mock import patch
 
 import pytest
-from outcome.utils.config import Config, FeatureException, FeatureSet, is_valid_feature_name
+from outcome.utils import feature_set
+from outcome.utils.config import Config
 
 valid_feature_names_raw = [
     'feature',
@@ -18,6 +20,17 @@ invalid_feature_names_raw = [
     'feature_',
 ]
 
+expected_feature_keys = [
+    ('feat', 'WITH_FEAT_FEAT'),
+    ('my.feat', 'WITH_FEAT_MY_FEAT'),
+    ('my.nested.feat', 'WITH_FEAT_MY_NESTED_FEAT'),
+]
+
+
+@pytest.mark.parametrize('feature,key', expected_feature_keys)
+def test_feature_env_keys(feature, key):
+    assert feature_set._feature_to_env_key(feature) == key
+
 
 @pytest.fixture(params=valid_feature_names_raw)
 def valid_feature_name(request):
@@ -30,86 +43,133 @@ def invalid_feature_name(request):
 
 
 def test_valid_feature_names(valid_feature_name):
-    assert is_valid_feature_name(valid_feature_name)
+    assert feature_set._is_valid_feature_name(valid_feature_name)
 
 
 def test_invalid_feature_names(invalid_feature_name):
-    assert not is_valid_feature_name(invalid_feature_name)
+    assert not feature_set._is_valid_feature_name(invalid_feature_name)
 
 
-@pytest.fixture()
-def feature_set():
-    fs = FeatureSet()
+boolean_true_values_raw = [
+    't',
+    'y',
+    '1',
+    'true',
+    'yes',
+    'YES',
+    'TRUE',
+    1,
+    True,
+]
 
-    fs.register_feature('my_active_feature', default=True)
-    fs.register_feature('my_inactive_feature', default=False)
 
-    return fs
+boolean_false_values_raw = [
+    'f',
+    'n',
+    '0',
+    'false',
+    'no',
+    'NO',
+    False,
+    0,
+]
+
+
+@pytest.fixture(autouse=True)
+def reset_features():
+    feature_set.reset()
+
+
+@pytest.fixture(params=boolean_true_values_raw)
+def truth_value(request):
+    return request.param
+
+
+@pytest.fixture(params=boolean_false_values_raw)
+def false_value(request):
+    return request.param
+
+
+def test_coerce_truth_values(truth_value):
+    assert feature_set._coerce_boolean(truth_value)
+
+
+def test_coerce_false_values(false_value):
+    assert not feature_set._coerce_boolean(false_value)
 
 
 def test_invalid_registration():
-    fs = FeatureSet()
-    with pytest.raises(FeatureException):
-        fs.register_feature('_invalid')
+    with pytest.raises(feature_set.FeatureException):
+        feature_set.register_feature('_invalid')
 
 
+default_feature = 'default_feature'
+inactive_feature = 'inactive_feature'
+active_feature = 'active_feature'
+unknown_feature = 'unknown'
+
+feature_toml = Path(Path(__file__).parent, 'fixtures', 'sample.toml')
+feature_config = Config(feature_toml)
+
+expected_feature_states = [
+    (default_feature, False),
+    (inactive_feature, False),
+    (active_feature, True),
+]
+
+
+@pytest.fixture()
+def register_feature():
+    feature_set.register_feature(default_feature)
+    feature_set.register_feature(inactive_feature, default=False)
+    feature_set.register_feature(active_feature, default=True)
+
+
+@pytest.mark.usefixtures('register_feature')
 def test_duplicate_registration():
-    fs = FeatureSet()
-    fs.register_feature('feat')
-
-    with pytest.raises(FeatureException):
-        fs.register_feature('feat')
+    with pytest.raises(feature_set.FeatureException):
+        feature_set.register_feature(default_feature)
 
 
 @patch.dict(os.environ, {}, clear=True)
-class TestFeatureSetFromDefaults:
-    def test_unknown_feature(self, feature_set: FeatureSet):
-        with pytest.raises(FeatureException):
-            feature_set.is_active('unknown_feature')
-
-    def test_default_active_feature(self, feature_set: FeatureSet):
-        assert feature_set.is_active('my_active_feature')
-
-    def test_default_inactive_feature(self, feature_set: FeatureSet):
-        assert not feature_set.is_active('my_inactive_feature')
+@pytest.mark.usefixtures('register_feature')
+@pytest.mark.parametrize('feature,expected', expected_feature_states)
+def test_is_active(feature, expected):
+    assert feature_set.is_active(feature) == expected
 
 
-@patch.dict(os.environ, {'WITH_FEAT_MY_ACTIVE_FEATURE': '0', 'WITH_FEAT_MY_INACTIVE_FEATURE': '1'}, clear=True)
-class TestFeatureSetFromEnv:
-    def test_unknown_feature(self, feature_set: FeatureSet):
-        with pytest.raises(FeatureException):
-            feature_set.is_active('unknown_feature')
-
-    def test_deactivated_feature(self, feature_set: FeatureSet):
-        assert not feature_set.is_active('my_active_feature')
-
-    def test_activated_feature(self, feature_set: FeatureSet):
-        assert feature_set.is_active('my_inactive_feature')
+@pytest.mark.usefixtures('register_feature')
+def test_all_features():
+    assert feature_set.features() == dict(expected_feature_states)
 
 
-fixture_file = Path(Path(__file__).parent, 'fixtures', 'sample.toml')
+@patch.dict(os.environ, {'APP_ENV': 'dev'}, clear=True)
+@pytest.mark.usefixtures('register_feature')
+def test_unknown_feature_dev():
+    with warnings.catch_warnings(record=True) as w:
+        assert not feature_set.is_active(unknown_feature)
+        assert len(w) == 1
+        assert issubclass(w[-1].category, RuntimeWarning)
 
 
-@pytest.fixture()
-def config():
-    return Config(path=fixture_file)
+@patch.dict(os.environ, {'APP_ENV': 'production'}, clear=True)
+@pytest.mark.usefixtures('register_feature')
+def test_unknown_feature_prod():
+    with warnings.catch_warnings(record=True) as w:
+        with pytest.raises(feature_set.FeatureException):
+            assert not feature_set.is_active(unknown_feature)
+        assert len(w) == 0
 
 
-@pytest.fixture()
-def feature_set_with_config(config: Config):
-    config.feature_set.register_feature('my_active_feature')
-    config.feature_set.register_feature('my_inactive_feature')
-    return config.feature_set
+@patch.dict(os.environ, {'WITH_FEAT_DEFAULT_FEATURE': '1', 'WITH_FEAT_ACTIVE_FEATURE': '0'}, clear=True)
+@pytest.mark.usefixtures('register_feature')
+def test_is_active_from_env():
+    assert feature_set.is_active(default_feature)
+    assert not feature_set.is_active(active_feature)
 
 
 @patch.dict(os.environ, {}, clear=True)
-class TestFeatureSetFromPyProject:
-    def test_unknown_feature(self, feature_set_with_config: FeatureSet):
-        with pytest.raises(FeatureException):
-            feature_set_with_config.is_active('unknown_feature')
-
-    def test_deactivated_feature(self, feature_set_with_config: FeatureSet):
-        assert not feature_set_with_config.is_active('my_active_feature')
-
-    def test_activated_feature(self, feature_set_with_config: FeatureSet):
-        assert feature_set_with_config.is_active('my_inactive_feature')
+@pytest.mark.usefixtures('register_feature')
+def test_is_active_from_toml():
+    feature_set.set_config(feature_config)
+    assert feature_set.is_active(default_feature)
