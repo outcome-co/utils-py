3e211c6938a17f83571cb7b1b13321ee16aafc94
Flore de Lasteyrie <41300908+floredelasteyrie@users.noreply.github.com>
2020-10-09 09:46:00 +0200

    feat: add sync and async cache (#63)
    * feat: add cache and async cache
    
    * chore: make check

---
+++ test/test_cache.py
@@ -0,0 +1,104 @@
+from unittest.mock import patch
+
+import asynctest
+import pytest
+from dogpile.cache.api import NO_VALUE
+from outcome.utils import cache
+
+test = 'test'
+
+
+class TestAsyncCache(asynctest.TestCase):
+    region = cache.get_cache_region()
+    cache.configure_cache_region(region, settings={}, prefix=test)
+
+    @region.cache_on_arguments()
+    async def non_cacheable_async_func(self):
+        return test
+
+    @region.cache_on_arguments()
+    @cache.cache_async
+    async def cacheable_async_func(self):
+        return test
+
+    async def test_cache_async_success(self):
+        f = self.cacheable_async_func()
+        assert await f == test
+        assert await f == test
+
+    async def test_cache_async_fail(self):
+        f = self.non_cacheable_async_func()
+        assert await f == test
+        with pytest.raises(RuntimeError):
+            assert await f == test
+
+
+def dummy_cached_fn(**kwargs):
+    return True
+
+
+class DummyClass:
+    def dummy_cached_fn(self):
+        return True
+
+
+module = __name__
+
+
+def test_key_generator_no_namespace():
+    prefix = f'{module}:{dummy_cached_fn.__name__}|'
+    generator = cache.cache_key_generator(namespace=None, fn=dummy_cached_fn)
+    key = generator()
+
+    assert key.startswith(prefix)
+
+
+def test_key_generator_namespace():
+    prefix = f'{module}:{dummy_cached_fn.__name__}|ns|'
+    generator = cache.cache_key_generator(namespace='ns', fn=dummy_cached_fn)
+    key = generator()
+
+    assert key.startswith(prefix)
+
+
+def test_key_generator_with_kwargs():
+    generator = cache.cache_key_generator(namespace=None, fn=dummy_cached_fn)
+
+    with pytest.raises(ValueError):
+        generator(foo='bar')
+
+
+def test_key_generator_strip_self():
+
+    inst_a = DummyClass()
+    inst_b = DummyClass()
+
+    generator_a = cache.cache_key_generator(namespace=None, fn=inst_a.dummy_cached_fn)
+    generator_b = cache.cache_key_generator(namespace=None, fn=inst_b.dummy_cached_fn)
+
+    # Since the methods are bound to the instances, the first argument
+    # is `self`, which should be different - we want to ensure we've
+    # detected and removed the `self` arg from the key calculation
+    assert generator_a(1) == generator_b(1)
+
+
+@patch('outcome.utils.cache.TTLCache', autospec=True)
+def test_ttl_interface(mocked_ttlcache):
+    init_args = {'maxsize': 100, 'ttl': 200}
+    backend = cache.TTLBackend(init_args)
+
+    mocked_ttlcache.assert_called_with(**init_args)
+
+    key = 'key'
+    value = 'value'
+
+    backend.delete(key)
+    backend.cache.pop.assert_called_with(key)
+    backend.cache.reset_mock()
+
+    backend.set(key, value)
+    backend.cache.__setitem__.assert_called_with(key, value)
+    backend.cache.reset_mock()
+
+    backend.get(key)
+    backend.cache.get.assert_called_with(key, NO_VALUE)
